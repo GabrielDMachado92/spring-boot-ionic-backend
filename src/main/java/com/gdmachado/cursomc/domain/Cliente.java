85bcf2da6ad17fefd6819e7a94864621cc7cb2b6
Gabriel Machado <gabriel.d.machado92@gmail.com>
2018-08-02 16:21:42 -0300

    usando somentoe  JsonIgnore

--- src/main/java/com/gdmachado/cursomc/domain/Cliente.java
+++ src/main/java/com/gdmachado/cursomc/domain/Cliente.java
@@ -14,8 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.gdmachado.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -35,7 +34,6 @@ public class Cliente implements Serializable{
 	private Integer tipo;
 	
 	@OneToMany(mappedBy="cliente")
-	@JsonManagedReference
 	private List<Endereco> enderecos = new ArrayList<>();
 	
 	@ElementCollection
@@ -43,7 +41,7 @@ public class Cliente implements Serializable{
 	private Set<String> telefones = new HashSet<>();
 	
 	@OneToMany(mappedBy="cliente")
-	@JsonBackReference
+	@JsonIgnore
 	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {}
